ffe7a9d97ef7f4c47e78e2573025cddb47e7b5f2
peasantdev <peasantwebdev@outlook.com>
2023-03-14 00:54:06 -0700

    Add project files.

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const frameHeight = 102;
+const frames = 15;
+const div = document.getElementById("animation");
+let frame = 0;
+setInterval(function () {
+    const frameOffset = (++frame % frames) * -frameHeight;
+    div.style.backgroundPosition = "0px " + frameOffset + "px";
+}, 100);
